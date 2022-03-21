bea856b8a8fdc25fead042a5d5a4e876674dd222
Robert <robpietrzak@yahoo.com>
2022-03-20 23:52:23 -0400

    Challenge commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector2.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        transform.position = Vector2.Lerp(startMarker.position, endMarker.position, Mathf.PingPong(fracJourney, 1));
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,11 +11,36 @@ public class PlayerScript : MonoBehaviour
     public Text score;
     private int scoreValue = 0;
 
+    public Text lives;
+    private int livesValue = 3;
+
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
+    public AudioSource musicSource;
+
+    private bool gameOver;
+    private bool hasTeleported;
+    private bool facingRight = true;
+    private bool airborn;
+
+    Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
+        hasTeleported = false;
+        gameOver = false;
         rd2d = GetComponent<Rigidbody2D>();
-        score.text = scoreValue.ToString();
+        score.text = "Score: " + scoreValue.ToString();
+        lives.text = "Lives: " + livesValue.ToString();
+        winTextObject.SetActive(false);
+        loseTextObject.SetActive(false);
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +50,7 @@ public class PlayerScript : MonoBehaviour
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
+        
 
     }
 
@@ -33,23 +59,88 @@ public class PlayerScript : MonoBehaviour
         if(collision.collider.tag == "Coin")
         {
             scoreValue++;
-            score.text = scoreValue.ToString();
+            score.text = "Score: " + scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+        }
+        if(collision.collider.tag == "Enemy")
+        {
+            livesValue--;
+            lives.text = "Lives: " + livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+        }
+        if(scoreValue == 4 && hasTeleported == false)
+        {
+            transform.position = new Vector2(50.0f, 1.0f);
+            hasTeleported = true;
+        }
+        if(scoreValue == 8 && gameOver == false)
+        {
+            musicSource.loop = false;
+            musicSource.Stop();
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            winTextObject.SetActive(true);
+            gameOver = true;
         }
     }
 
     void OnCollisionStay2D(Collision2D collision)
     {
+        float verMovement = Input.GetAxis("Vertical");
         if(collision.collider.tag == "Ground")
         {
+            if(verMovement == 0)
+            {
+                airborn = false;
+            }
+            else if(verMovement > 0)
+            {
+                airborn = true;
+            }
             if(Input.GetKey(KeyCode.W))
             {
                 rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
             }
         }
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+
     void Update()
     {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float verMovement = Input.GetAxis("Vertical");
+        if(facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+        if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+        if(Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+        }
+        if(Input.GetKeyUp(KeyCode.W) && airborn == false)
+        {
+            anim.SetInteger("State", 2);
+        }     
+        if(livesValue == 0)
+        {
+            Destroy(this);
+            loseTextObject.SetActive(true);
+        }
         if (Input.GetKey("escape"))
         {
             Application.Quit();
